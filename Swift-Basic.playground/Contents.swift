69b83724a3d53670f50651726ee8bc1138ff4517
yogigooy <96382106+yogigooy@users.noreply.github.com>
2021-12-19 21:48:07 +0700

    hey this my first commit :)

---
+++ Swift-Basic.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var color = "blue"
+
+print(color)
+
+
+//var str1 = """
+//This goes
+//over multiple
+//lines
+//"""
+//
+//
+//print(str1)
+
+
+var score = 85
+var str = "Your score was \(score)"
